804b20e9717ada26c16888ae73758e337285163e
Amanda <amanda212.chen@gmail.com>
2019-09-24 16:34:01 +0800

    initial commit

---
+++ app/src/main/java/com/amanda/bmi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amanda.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.box);
+    }
+}
